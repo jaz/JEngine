d157b28301c1380d8bd7023290c9e5f79b117764
Jacek Zagorski <jaz@shastanetworks.com>
2010-06-18 12:22:11 -0700

    remove unneeded things

--- JEngine4/org/jengine/core/ThrowableHandler.java
+++
@@ -1,6 +0,0 @@
-package org.jengine.core;
-
-public interface ThrowableHandler
-{
-	public void handleThrowable (Thread thread, Throwable throwable);
-}
--- JEngine4/org/jengine/ex/ShastaException.java
+++
@@ -1,9 +0,0 @@
-package org.jengine.ex;
-
-public class ShastaException extends RuntimeException
-{
-	public ShastaException(String message, Throwable cause)
-	{
-		super(message, cause);
-	}
-}
